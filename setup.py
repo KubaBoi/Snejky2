7f733e3a9516d9168f165a3664f0d475a4e83083
KubaBoi <jakubanderle@outlook.cz>
2021-03-26 00:15:06 +0100

    vsechno funguje

--- setup.py
+++ setup.py
@@ -1,18 +1,29 @@
 from setuptools import setup
 from Cython.Build import cythonize
 from os import walk
-from shutil import copyfile, move
+from shutil import copyfile, move, rmtree
 import os
 import os.path
 import time
 
 path = "EngineSource"
-distPath = "Engine"
+distPath = "EnginePyx"
+cPath = "Engine"
+
+#odstrani predchozi build
+if (os.path.exists(distPath)):
+    print(f"Removing {distPath}")
+    rmtree(distPath)
+
+if (os.path.exists(cPath)):
+    print(f"Removing {cPath}")
+    rmtree(cPath)
 
 #zkopiruje zdrojove kody a zmeni priponu na .pyx
 for (dirpath, dirnames, filenames) in walk(path):
     try:
         os.mkdir(dirpath.replace(path, distPath))
+        os.mkdir(dirpath.replace(path, cPath))
     except:
         print(dirpath)
 
@@ -23,7 +34,10 @@ for (dirpath, dirnames, filenames) in walk(path):
 for (dirpath, dirnames, filenames) in walk(distPath):
     for f in filenames:
         setup(ext_modules = cythonize(dirpath + "\\" + f))
-        name = f.replace(".pyx", "") + ".cp39-win_amd64.pyd"
+
+        name = f.replace("pyx", "") + "cp39-win_amd64.pyd"
+        cName = f.replace("pyx", "c")
         while (not os.path.exists(name)):
             time.sleep(0.1)
-        move(name, dirpath + "\\" + name)
+        move(name, dirpath.replace(distPath, cPath) + "\\" + name) #move .pyd soubory
+        move(dirpath + "\\" + cName, dirpath.replace(distPath, cPath) + "\\" + cName) #move .c soubory
