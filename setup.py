cc0698d14a9d8b4ec5473c5db24384b567ee336d
KubaBoi <jakubanderle@outlook.cz>
2021-03-25 23:50:37 +0100

    funkcni preklad do c++

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup
 from Cython.Build import cythonize
 from os import walk
-from shutil import copyfile
+from shutil import copyfile, move
 import os
+import os.path
+import time
 
 path = "EngineSource"
 distPath = "Engine"
 
+#zkopiruje zdrojove kody a zmeni priponu na .pyx
 for (dirpath, dirnames, filenames) in walk(path):
     try:
         os.mkdir(dirpath.replace(path, distPath))
@@ -16,7 +19,11 @@ for (dirpath, dirnames, filenames) in walk(path):
     for f in filenames:
         copyfile(dirpath + "\\" + f, dirpath.replace(path, distPath) + "\\" + f + "x")
 
-
+#vytvori .c soubory
 for (dirpath, dirnames, filenames) in walk(distPath):
     for f in filenames:
         setup(ext_modules = cythonize(dirpath + "\\" + f))
+        name = f.replace(".pyx", "") + ".cp39-win_amd64.pyd"
+        while (not os.path.exists(name)):
+            time.sleep(0.1)
+        move(name, dirpath + "\\" + name)
