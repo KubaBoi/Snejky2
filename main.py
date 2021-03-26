8fb42943bc8ef6948bdd8fd0b2e128748a5f3e18
KubaBoi <jakubanderle@outlook.cz>
2021-03-27 00:25:15 +0100

    tucnak

--- main.py
+++ main.py
@@ -3,21 +3,29 @@ from Engine.vector import Vector
 
 from Engine.object import Object
 from Engine.objects.cube import Cube
+from Engine.objects.pyramid import Pyramid
+from Engine.objects.penguin import Penguin
 import time
 
 engine = Engine()
 
 cube1 = Cube(Vector(-5, -5, 0), 1)
-engine.addObject(cube1)
-loadedCube = Object(Vector(-5, 5, 0))
-loadedCube.loadMesh("Engine/meshes/cube.json")
-engine.addObject(loadedCube)
+#engine.addObject(cube1)
+
+pyramid = Pyramid(Vector(-5, 0, 0), 2, color=(200, 30, 30))
+#engine.addObject(pyramid)
+
+#loadedObject = Object(Vector(0, 0, -10))
+#loadedObject.loadMesh("Engine/meshes/penguin.json")
+#engine.addObject(loadedObject)
+
+penguin = Penguin(Vector(0,0,-10), 1, engine)
+engine.addObject(penguin)
 
 run = True
 while run:
     #t = time.time()
     cube1.Position.x += 0.005
-    loadedCube.Position.x += 0.005
     engine.update()
 
     #print("Time: " + str(time.time() - t))
